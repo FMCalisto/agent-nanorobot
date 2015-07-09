08acb778defe66a5da6b3a583864d8128f441a3e
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-07-09 16:17:17 +0100

    Update nanorobot.lisp

--- src/nanorobot.lisp
+++ src/nanorobot.lisp
@@ -1,9 +1,8 @@
 (load "cell.lisp")
 
 (defun nanorobot (cell)
-  (cond ((cell-cancer cell) 'ATTACK)
-       ((cell-red-globule cell) 'RECHARGE)
-       ((cell-min-engine cell) 'STAY)
+  (cond ((cell-cancer-p cell) 'ATTACK)
+       ((cell-red-globule-p cell) 'RECHARGE)
        (T 'WAIT)
   )
 )
